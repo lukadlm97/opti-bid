de3ccccdd46a271c196a525cf612c56998a2b9cf
lukadlm97@gmail.com <lukadlm97@gmail.com>
2023-02-16 01:30:43 +0100

    investigate otp-Net library, but we should use google authenticator

---
+++ OptiBid.Console.Otp-Net/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+using OtpNet;
+
+var key = Guid.NewGuid().ToString();
+
+var otp = new Totp(Encoding.UTF8.GetBytes(key), 120, OtpHashMode.Sha512, 6);
+
+var topt =otp.ComputeTotp(DateTime.Now);
+Console.WriteLine(otp.RemainingSeconds());
+
+
+
+
+
+
+Console.ReadLine();
