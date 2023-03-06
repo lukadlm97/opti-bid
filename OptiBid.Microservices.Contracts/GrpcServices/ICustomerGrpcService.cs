6471443e636fa18cd3c76dd79081256b94e314a7
lukadlm97@gmail.com <lukadlm97@gmail.com>
2023-03-06 22:45:12 +0100

    Added missing methods and cancellation token

--- OptiBid.Microservices.Contracts/GrpcServices/ICustomerGrpcService.cs
+++ OptiBid.Microservices.Contracts/GrpcServices/ICustomerGrpcService.cs
@@ -6,7 +6,7 @@ namespace OptiBid.Microservices.Contracts.GrpcServices
     public interface ICustomerGrpcService
     {
         Task<CustomerDetailsResult> GetCustomerDetails(CustomerRequest customerRequest,CancellationToken cancellationToken=default);
-        Task<CustomerResult> Create(CustomerRequest customerRequest, CancellationToken cancellationToken = default));
-        Task<IEnumerable<CustomerDetailsResult>> GetAll( CancellationToken cancellationToken = default));
+        Task<CustomerResult> Create(CustomerRequest customerRequest, CancellationToken cancellationToken = default);
+        Task<IEnumerable<CustomerDetailsResult>> GetAll( CancellationToken cancellationToken = default);
     }
 }
